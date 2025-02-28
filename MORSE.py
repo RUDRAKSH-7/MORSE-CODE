826876d897fee1a0d752fc976b7330a5d7ea7fe8
RUDRAKSH <119749657+RUDRAKSH-7@users.noreply.github.com>
2025-02-28 15:30:04 +0530

    Update MORSE.py

--- MORSE.py
+++ MORSE.py
@@ -1,4 +1,14 @@
-from colorama import Fore, Style
+try:
+    from colorama import Fore, Style ;
+    green = Style.BRIGHT+Fore.LIGHTGREEN_EX
+    cyan = Style.BRIGHT+Fore.CYAN
+    red = Style.BRIGHT+Fore.RED
+    yellow = Style.BRIGHT+Fore.YELLOW
+    white = Style.BRIGHT+Fore.WHITE
+    reset = Style.RESET_ALL
+    magenta = Style.BRIGHT+Fore.MAGENTA
+except ImportError or NameError:
+    green = cyan = red = yellow = white = magenta = reset = ""
 from os import system ; import time as t
 
 chars = [
@@ -17,52 +27,46 @@ chars = [
 'y','-.--','z','--..'
 ]
 
-green = Style.BRIGHT+Fore.LIGHTGREEN_EX
-cyan = Style.BRIGHT+Fore.CYAN
-red = Style.BRIGHT+Fore.RED
-yellow = Style.BRIGHT+Fore.YELLOW
-white = Style.BRIGHT+Fore.WHITE
-reset = Style.RESET_ALL
-
-def morse_to_english(code):
-    if not "  " in code:
-        string = str(code).split(" ")
-        result = ""
-        print(f"\n{cyan}STRING -> {Style.RESET_ALL}",end="")
-        for i in string:
-            result += chars[chars.index(i)-1]  
-        print(green+result)
-    else:
-        string = str(code).split("  ")
-        result = ""
-        print(f"\n{cyan}STRING -> ",end="")
-        for i in string:
-            breakdown = str(i).split(" ")
-            for i in breakdown:
+class convert:
+    def morse_to_english(code):
+        if not "  " in code:
+            string = str(code).split(" ")
+            result = ""
+            print(f"\n{cyan}STRING -> {reset}",end="")
+            for i in string:
                 result += chars[chars.index(i)-1]  
-            result += " "
-        print(green+result)
+            print(green+result)
+        else:
+            string = str(code).split("  ")
+            result = ""
+            print(f"\n{cyan}STRING -> ",end="")
+            for i in string:
+                breakdown = str(i).split(" ")
+                for i in breakdown:
+                    result += chars[chars.index(i)-1]  
+                result += " "
+            print(green+result)
 
-def english_to_morse(string):
-    if not " " in string:
-        result= ""
-        print(f"\n{cyan}CODE -> ",end = " ")
-        for i in string:
-            result += chars[chars.index(i)+1]+" "
-        print(result)
-    else:
-        code = str(string).split(" ")
-        result = ""
-        print("\n{cyan}CODE -> ",end = " ")
-        for i in code:
-            for j in i:
-                result += chars[chars.index(j)+1]+" "
-            result += " "
-        print(result)
+    def english_to_morse(string):
+        if not " " in string:
+            result= ""
+            print(f"\n{cyan}CODE -> ",end = " ")
+            for i in string:
+                result += chars[chars.index(i)+1]+" "
+            print(result)
+        else:
+            code = str(string).split(" ")
+            result = ""
+            print("\n{cyan}CODE -> ",end = " ")
+            for i in code:
+                for j in i:
+                    result += chars[chars.index(j)+1]+" "
+                result += " "
+            print(result)
 
 while True:
     print(f"{cyan}              MORSE CONVERTER : \n")
-    print(f"{Fore.MAGENTA+Style.BRIGHT}1. ENG --> MORSE           {red}2. MORSE --> ENGLISH{reset}")
+    print(f"{magenta}1. ENG --> MORSE           {red}2. MORSE --> ENGLISH{reset}")
     try:
         choice = int(input(F"\n{green}OPTION [ 1 ] | [ 2 ] : {white}"))
         if choice > 2:
@@ -79,12 +83,27 @@ while True:
         continue
     if choice == 1:
         system('cls')
-        string = str(input(f"{green}STRING : {white}"))
-        english_to_morse(string)
-        print(reset+"\n\n\n\n\n\n")
+        try:
+            string = str(input(f"{green}STRING : {white}"))
+            convert.english_to_morse(string)
+            print(reset+"\n\n\n\n\n\n")
+        except ValueError:
+            system('cls')
+            print(F"{red}# RETRY")
+            t.sleep(1)
+            system('cls')
+            continue
     elif choice == 2:
         system('cls')
-        code = str(input(f"{cyan}CODE : {green}"))
-        morse_to_english(code)
-        print(reset+"\n\n\n\n\n\n")
+        try:
+            code = str(input(f"{cyan}CODE : {green}"))
+            convert.morse_to_english(code)
+            print(reset+"\n\n\n\n\n\n")
+        except ValueError:
+            system('cls')
+            print(F"{red}# RETRY")
+            t.sleep(1)
+            system('cls')
+            continue
+    input("press enter to quit...")
     quit()
