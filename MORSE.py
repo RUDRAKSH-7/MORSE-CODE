48f953c88549580179c9eb8937af67df858838c9
RUDRAKSH <119749657+RUDRAKSH-7@users.noreply.github.com>
2025-03-07 21:28:38 +0530

    Update MORSE.py

--- MORSE.py
+++ MORSE.py
@@ -7,20 +7,21 @@ try:
     white = Style.BRIGHT+Fore.WHITE
     reset = Style.RESET_ALL
     magenta = Style.BRIGHT+Fore.MAGENTA
+    grey = Style.BRIGHT+Fore.BLACK
 except ImportError or NameError:
     green = cyan = red = yellow = white = magenta = reset = ""
 from os import system ; import time as t
 
 chars = [
-'a','.-',    'b','-...',    'c','-.-.',    'd','-..',
-'e','.',     'f','..-.',    'g','--.',     'h','....',
-'i','..',    'j','.---',    'k','-.-',     'l','.-..',
-'m','--',    'n','-.',      'o','---',     'p','.--.',
-'q','--.-',  'r','.-.',     's','...',     't','-',
-'u','..-',   'v','...-',    'w','.--',     'x','-..-',
-'y','-.--',  'z','--..',    '1','.----',   '2',"..---",
-'3','...--', '4','....-',   '5','.....',   '6','-....',
-'7','--...', '8','---..',   '9','----.',   '0','-----'
+'a','.-',       'b','-...',     'c','-.-.',    'd','-..',
+'e','.',        'f','..-.',     'g','--.',     'h','....',
+'i','..',       'j','.---',     'k','-.-',     'l','.-..',
+'m','--',       'n','-.',       'o','---',     'p','.--.',
+'q','--.-',     'r','.-.',      's','...',     't','-',
+'u','..-',      'v','...-',     'w','.--',     'x','-..-',
+'y','-.--',     'z','--..',     '1','.----',   '2',"..---",
+'3','...--',    '4','....-',    '5','.....',   '6','-....',
+'7','--...',    '8','---..',    '9','----.',   '0','-----'
 ]
 
 class convert:
@@ -60,15 +61,32 @@ class convert:
                 result += " "
             print(result)
 
+def instructions():
+    system('cls')
+    print(white+"1. String should only be numbers or English alphabets.")
+    print("2. Morse code letters should also follow the above instruction")
+    print("3. While writing morse code, letters are to be seperated using one space.")
+    print("4. Words are to be separated using double spaces.")
+    print("5. English string should not contain any punctuations")
+    input("\n\n                 press enter...")
+    return
+
+
 while True:
+    print("\n\n\n\n\n\n")
     print(f"{cyan}              MORSE CONVERTER : \n")
-    print(f"{magenta}1. ENG --> MORSE           {red}2. MORSE --> ENGLISH{reset}")
+    print(f"{magenta}1. ENG --> MORSE           {red}2. MORSE --> ENGLISH{reset}\n\n                  3. EXIT")
+    print(f"{grey}\n       # press 'i' for instructions")
     try:
-        choice = int(input(F"\n{green}OPTION [ 1 ] | [ 2 ] : {white}"))
-        if choice > 2:
+        choice = str(input(F"\n{green}OPTION [1]  [2]  [3] : {white}"))
+        if ord(choice) > ord('3') and ord(choice) < ord('1'):
             system('cls')
             print(F"{red}# RETRY")
             t.sleep(1)
+            system('cls')
+            continue
+        elif choice.lower() == 'i':
+            instructions()
             system('cls')
             continue
     except ValueError:
@@ -77,29 +95,30 @@ while True:
         t.sleep(1)
         system('cls')
         continue
-    if choice == 1:
+    if choice == '1':
         system('cls')
         try:
             string = str(input(f"{green}STRING : {white}"))
             convert.english_to_morse(string)
-            print(reset+"\n\n\n\n\n\n")
+            print(reset)
         except ValueError:
             system('cls')
             print(F"{red}# RETRY")
             t.sleep(1)
             system('cls')
             continue
-    elif choice == 2:
+    elif choice == '2':
         system('cls')
         try:
             code = str(input(f"{cyan}CODE : {green}"))
             convert.morse_to_english(code)
-            print(reset+"\n\n\n\n\n\n")
+            print(reset)
         except ValueError:
             system('cls')
             print(F"{red}# RETRY")
             t.sleep(1)
             system('cls')
             continue
-    input("press enter to quit...")
-    quit()
+    elif choice == '3':
+        break
+quit()
